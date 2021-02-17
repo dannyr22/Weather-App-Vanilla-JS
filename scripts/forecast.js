19ec078963504692d0bdafdafa67bca7b5d8b320
dannyr22 <dannyr2763@hotmail.com>
2021-02-17 12:09:19 +0000

    initial commit

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = '0xGY0xGuwgik3srKabTIKBTaZW1AXMFG'
+
+//get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const res = await fetch(base + query);
+    const data = await res.json();
+
+    return (data[0])
+
+
+};
+
+//get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+
+
+    const res = await fetch(base + query)
+    const data = await res.json()
+
+    return data[0]
+} 
+
